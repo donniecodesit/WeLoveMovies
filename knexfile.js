a9ccce2436e804d21ed30919608b9525e45aa2cb
Donovan Laws <donovan.laws@gmail.com>
2022-01-31 11:08:50 -0700

    Readded test environment

--- knexfile.js
+++ knexfile.js
@@ -27,5 +27,20 @@ module.exports = {
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-  }
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    
+    useNullAsDefault: true
+  },
 };
